4cc469064ecbf362da6075cf7965e0976b8f669d
mbosley <37943910+mbosley@users.noreply.github.com>
2020-12-11 10:55:08 -0800

    refractored scraping code

--- code/scrape_links.jl
+++ code/scrape_links.jl
@@ -1,22 +1,26 @@
 using Gumbo
 using Cascadia
 using HTTP
+using Distributed
 
 url_base = "https://eparlib.nic.in"
 range = Integer.(7e5:8e5)
 
-@time begin
-    Threads.@threads for n in 760000:761000
-        println("trying $n on thread $(Threads.threadid())")
-        try
-            r = HTTP.get("https://eparlib.nic.in/handle/123456789/$n"; readtimeout = 2, retry = false)
-            h = parsehtml(String(r.body))
-            link = eachmatch(Selector(".btn-primary"), h.root)[1].attributes["href"]
-            open("data/raw/links.txt", "a") do file
-                write(file, "\n" * url_base * link)
-            end
-        catch e
-            @info e
+"""
+Gets link from Indian congressional record website.
+"""
+function get_link(n::Integer)
+    try
+        r = HTTP.get("https://eparlib.nic.in/handle/123456789/$n"; readtimeout = 2, retry = false)
+        h = parsehtml(String(r.body))
+        link = eachmatch(Selector(".btn-primary"), h.root)[1].attributes["href"]
+        println("adding $(url_base * link) to links file")
+        open("data/raw/links.txt", "a") do file
+            write(file, "\n" * url_base * link)
         end
+    catch e
+        @info e
     end
 end
+
+pmap(x -> get_link(x), range)
